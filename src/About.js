10ad444c9e5884e4e0c087e556c3ffb6cd252d73
windows <windows@microsoft.com>
2023-09-24 10:35:07 +0530

    notes

---
+++ src/About.js
@@ -0,0 +1,16 @@
+function Myabout()
+{
+    return "this is about page";
+}
+export default Myabout
+
+
+export function Mycontact()
+{
+    return "this is contactus page";
+}
+
+export function Service()
+{
+    return <h1>this is server page</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import India,{Mycontact,Service} from './About';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <India></India>
+    <h1>this is computer</h1>
+    <Mycontact></Mycontact>
+    <Service></Service>
   </React.StrictMode>
 );
 
