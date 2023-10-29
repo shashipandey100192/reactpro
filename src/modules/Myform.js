15fe4402c3888f5efab505e01cca3532f286a630
windows <windows@microsoft.com>
2023-10-29 10:35:41 +0530

    change

--- src/modules/Myform.js
+++ src/modules/Myform.js
@@ -1,20 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useForm, SubmitHandler } from "react-hook-form";
 
+var mydatalist=[];
 function Myform() {
-const [svariable,sfunc]=useState("kumar")
+// const [svariable,sfunc]=useState("kumar")
+// const changevalue = (myelement)=>{
+//   console.log(svariable);
+//   sfunc(myelement.target.value);
+// }
 
-const changevalue = (myelement)=>{
-  console.log(svariable);
-  sfunc(myelement.target.value);
+
+const {register,handleSubmit,formState: { errors }} = useForm()
+const mysubmit = (dobject)=>
+{
+  console.log(dobject);
+  mydatalist.push(dobject);
+  console.log(mydatalist);
+  localStorage.setItem("formdata",JSON.stringify(mydatalist));
+
 }
 
-const [jcolor,jfunc]=useState("red");
+
+const [jcolor,jfunc]=useState("lightgray");
 const mycolor = (e)=>{
   jfunc(e.target.value)
 }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(mysubmit)}>
     <div className='container border p-5 shadow cus-form' style={{backgroundColor:`${jcolor}`}}>
       <div className='row'>
         <div className='col-12'>
@@ -24,29 +37,29 @@ const mycolor = (e)=>{
       <div className='row'>
         <div className='col-md-6'>
           <label className="form-label">fullName</label>
-          <input type="text" className="form-control" value={svariable} onChange={changevalue}/>
-          <div className="form-text">anyone else.</div>
+          <input type="text" className="form-control" {...register("fullname",{ required: true,pattern:/^[A-Za-z]+$/i})} name='fullname'/>
+          {errors.fullname && <span className='text-danger h6'>full name is required</span>}
         </div>
         <div className='col-md-6'>
           <label className="form-label">Email address</label>
-          <input type="email" className="form-control"/>
-          <div className="form-text">anyone else.</div>
+          <input type="email" className="form-control" {...register("emailid",{ required: true })} name='emailid'/>
+          {errors.emailid && <span className='text-danger h6'>email is required</span>}
         </div>
         <div className='col-md-6'>
           <label className="form-label">Phone No</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...register("phoneno")} name='phoneno'/>
           <div className="form-text">anyone else.</div>
         </div>
         <div className='col-md-6'>
           <label className="form-label">Gender</label>
           <section className="mt-2">
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="radio" name="a" />
-            <label className="form-check-label">1</label>
+            <input className="form-check-input" type="radio" value="male" name="a" {...register("gender")} />
+            <label className="form-check-label">male</label>
           </div>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="radio" name="a" />
-            <label className="form-check-label">2</label>
+            <input className="form-check-input" type="radio" value="female" name="a" {...register("gender")}/>
+            <label className="form-check-label">female</label>
           </div>
           </section>
 
@@ -54,17 +67,17 @@ const mycolor = (e)=>{
 
         <div className='col-md-12'>
           <label className="form-label">Comments</label>
-          <textarea className="form-control"></textarea>
+          <textarea className="form-control" {...register("comment")} name='comment'></textarea>
           <div className="form-text">anyone else.</div>
         </div>
         <div className='col-md-6'>
           <label className="form-label">password</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...register("pass")} name='pass'/>
           <div className="form-text">anyone else.</div>
         </div>
         <div className='col-md-6'>
           <label className="form-label">repassword</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...register("repass")} name='repass'/>
           <div className="form-text">anyone else.</div>
         </div>
         <div className='col-md-12 text-center p-3'>
--- src/modules/services/NewLeptop.js
+++ src/modules/services/NewLeptop.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function NewLeptop() {
+  const [mydata,mydatafunc]=useState([])
+
+    const myapi = ()=>{
+        axios.get('https://dummyjson.com/products').then((res)=>{
+        console.log(res.data.products);
+        mydatafunc(res.data.products);
+        })
+    }
+
+    useEffect(()=>{
+      myapi();
+    },[])
+
+
   return (
-    <div>NewLeptop</div>
+   <div className='container'>
+    <div className='row'>
+     
+      {mydata.map((d)=>{
+        return(
+      <div className='col-md-4'>
+        <div className='card p-2 shadow bg-light'>
+        <h4>{d.id}</h4>
+        <h4>{d.brand}</h4>
+        <h4>{d.category}</h4>
+        <img src={d.images[0]} width={200}/>
+      </div>
+      </div>
+
+        )
+
+      })}
+
+
+   
+    </div>
+   </div>
   )
 }
 
